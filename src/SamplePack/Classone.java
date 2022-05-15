2235133e13ff1a60a64b80c72f1f71498b4ed326
Satya <satyagokavarapu1@gmail.com>
2022-05-15 20:24:01 +0530

    first commit

---
+++ src/SamplePack/Classone.java
@@ -0,0 +1,14 @@
+package SamplePack;
+
+public class Classone {
+
+    public void clsones(){
+
+        System.out.println("clsones");
+
+
+
+
+
+    }
+}
---
+++ src/SamplePack/Classtow.java
@@ -0,0 +1,16 @@
+package SamplePack;
+
+public class Classtow {
+
+    public void clsones(){
+
+        System.out.println("clsone");
+
+    }
+    public void clstwo(){
+
+        System.out.println("clstwo");
+
+    }
+
+}
